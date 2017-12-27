c4922ebf5f99c305f273275f49ad616d83fc8b43
adashev <dimahelper@mail.ru>
2017-12-27 18:00:08 +0300

    Задание №2: функция distance в классе Point

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
@@ -3,18 +3,8 @@ package ru.stqa.pft.sandbox;
 public class Hello {
 
     public static void main(String[] args) {
-        Point point1 = new Point(2, 3);
         Point point2 = new Point(4, 5);
-
-        double dist = distance(point1, point2);
+        double dist = point2.distance(2, 3);
         System.out.println("расстояние между точками равно " + dist);
-
-    }
-
-    public static double distance(Point p1, Point p2){
-        double deltax = p2.x - p1.x;
-        double deltay = p2.y - p1.y;
-        double dist = Math.sqrt(deltax*deltax + deltay*deltay);
-        return dist;
     }
 }
--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,12 +1,17 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-
-  double x;
-  double y;
-
+  double x2;
+  double y2;
   public Point(double x, double y){
-    this.x = x;
-    this.y = y;
+    this.x2 = x; //вторая точка - объект, в котором метод вызывается
+    this.y2 = y;
+  }
+
+  public double distance(double x1, double y1){ //первая точка - передаётся в него как параметр
+    double deltax = this.x2 - x1;
+    double deltay = this.y2 - y1;
+    double dist = Math.sqrt(deltax*deltax + deltay*deltay);
+    return dist;
   }
 }
