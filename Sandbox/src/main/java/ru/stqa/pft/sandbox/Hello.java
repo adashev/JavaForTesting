25082de88950df334ad71fdb08c89df5f206e8f1
adashev <dimahelper@mail.ru>
2017-12-27 12:28:50 +0300

    добавлено несколько новых функций

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
@@ -1,7 +1,31 @@
 package ru.stqa.pft.sandbox;
 
 public class Hello {
+
     public static void main(String[] args) {
-        System.out.println("Hello, people!");
+        hello("world");
+        hello("user");
+        hello("dima");
+
+        double l = 5;
+        //double pl = area(len);
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 5;
+        double b = 4;
+        System.out.println("Площадь прямоуг. со сторонами " + a + " и " + b + " = " + area(a, b));
+
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
