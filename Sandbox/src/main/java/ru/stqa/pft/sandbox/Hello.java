a2cac54b79e77511c271e9232ecd1c6763e44dfe
adashev <dimahelper@mail.ru>
2018-01-02 15:52:07 +0300

    работа над ошибками в Задание №2: функция distance в классе Point

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
@@ -1,9 +1,12 @@
 package ru.stqa.pft.sandbox;
 
 public class Hello {
+
     public static void main(String[] args) {
         Point point2 = new Point(4, 5);
-        double dist = point2.distance(2, 3);
+        Point point1 = new Point(2, 3);
+
+        double dist = point2.distance(point1.x, point1.y);
         System.out.println("расстояние между точками равно " + dist);
     }
 }
--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,16 +1,16 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  double x2;
-  double y2;
+  double x;
+  double y;
   public Point(double x, double y){
-    this.x2 = x; //вторая точка - объект, в котором метод вызывается
-    this.y2 = y;
+    this.x = x; //вторая точка - объект, в котором метод вызывается
+    this.y = y;
   }
 
-  public double distance(double x1, double y1){ //первая точка передаётся в метод как параметр
-    double deltax = this.x2 - x1;
-    double deltay = this.y2 - y1;
+  public double distance(double x1, double y1){ //первая точка - передаётся в него как параметр
+    double deltax = this.x - x1;
+    double deltay = this.y - y1;
     double dist = Math.sqrt(deltax*deltax + deltay*deltay);
     return dist;
   }
