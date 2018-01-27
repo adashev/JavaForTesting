66e7a44eb524dc4798d0cd888045513fa1742133
adashev <dimahelper@mail.ru>
2018-01-27 19:31:43 +0300

    Лекция 4.7. Множества: неупорядоченные коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,47 +1,63 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
+    private final String id; //добавили в 4.7
     private final String name;
     private final String header;
     private final String footer;
 
-    public GroupData(String name, String header, String footer) {
+    public GroupData(String id, String name, String header, String footer) {
+        this.id = id;//добавили в 4.7
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
+    public GroupData(String name, String header, String footer) {//это второй конструктор. его добавили в 4.7
+        this.id = null;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public String getId() { //добавили в 4.7
+        return id;
+    }
+
     public String getName() {
         return name;
     }
-
     public String getHeader() {
         return header;
     }
-
     public String getFooter() {
         return footer;
     }
 
-    @Override // добавили в 4.6 в контексте сравнение списков групп в тесте Удаления(с помощью IDE)
-    public String toString() {
+    @Override
+    public String toString() { // в 4.7 добавили в него id
         return "GroupData{" +
-                "name='" + name + '\'' +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
                 '}';
     }
 
-    @Override // добавили в 4.6 в контексте сравнение списков групп в тесте Удаления (с помощью IDE)
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         GroupData groupData = (GroupData) o;
 
+        if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
         return name != null ? name.equals(groupData.name) : groupData.name == null;
     }
 
-    @Override // добавили в 4.6 в контексте сравнение списков групп в тесте Удаления (с помощью IDE). В 4.6 не используется
+    @Override
     public int hashCode() {
-        return name != null ? name.hashCode() : 0;
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,11 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-
+import java.util.HashSet;
 import java.util.List;
-
 
 public class GroupModificationTests extends TestBase {
     @Test
@@ -13,17 +11,24 @@ public class GroupModificationTests extends TestBase {
         app.getNavigationHelper().gotoGroupPage();
 
         if (!app.getGroupHelper().isThereAGroup()) {
-            app.getGroupHelper().createGroup(new GroupData("test10", null, null));
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
         //int before = app.getGroupHelper().getGroupCount(); //кол-во групп ДО модификации группы (добавлено 4.3; в 4.5 перенесено после проверки предусловий)
         List<GroupData> before = app.getGroupHelper().getGroupList();//считываем текущий список групп на странице до удаления группы (4.5)
         app.getGroupHelper().selectGroup(before.size() - 1);//параметр before - 1 добавляем в 4.4, чтобы изменять не первую, а последнюю группу в списке
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test-A", "test20", "test30"));
+
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test1", "testMOD", "testMOD");// в 4.7 вынесли new GroupData из fillGroupForm(...)
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnGroupPage();
         //int after = app.getGroupHelper().getGroupCount(); //кол-во групп ПОСЛЕ модификации данной группы (добавлено в 4.3)
         List<GroupData> after = app.getGroupHelper().getGroupList();//считываем  список групп на странице после созданий новой группы (4.5)
         Assert.assertEquals(after.size(), before.size());//проверка итогового кол-ва групп (добавлено в 4.3)
+
+        before.remove(before.size() - 1);//удаляем элемент модифицированный через UI из нашего списка (4.7)
+        before.add(group);//вместо удаленного элемента добавляем новый модифицированный (4.7)
+        Assert.assertEquals(new HashSet(before), new HashSet(after));//сверка списков групп(добавлено в 4.7)
+        //метод HashSet для преобразования списка во множество добавлен в начале 4.7
     }
 }
