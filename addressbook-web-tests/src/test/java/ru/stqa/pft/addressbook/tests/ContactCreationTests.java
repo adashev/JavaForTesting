deef9b1098461594033960ae02594ef9e8831d9a
adashev <dimahelper@mail.ru>
2018-02-06 13:35:21 +0300

    Контакты 4.9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,9 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,7 +14,7 @@ public class ContactCreationTests extends TestBase {
         List<ContactData> before = app.getContactHelper().getContactList();//считываем текущий список контактов на страницы до создания нового (4.5)
         app.getNavigationHelper().gotoAddContactPage();
 
-        ContactData contact = new ContactData("creat", "О", "gr1", "Еж, 1", "4-0", "m@ru");//в 4.8
+        ContactData contact = new ContactData("creat12", "О", "gr1", "Еж, 1", "4-0", "m@ru");//в 4.8
         app.getContactHelper().createContact(contact,true);
         app.getNavigationHelper().gotoHomePage();
 
@@ -24,13 +23,18 @@ public class ContactCreationTests extends TestBase {
         Assert.assertEquals(after.size(), before.size() + 1);//проверка кол-ва контактов после создания нового (в рамках 4.3)
 
 
-        int max = 0;
+        /*int max = 0;
         for (ContactData g : after){ //в 4.8 добавили цикл поиска максимального id в списке контактов ПОСЛЕ добавления нового
             if (g.getId() > max){
                 max = g.getId();
             }
-        }
-        contact.setId(max); //добавлено по мотивам 4.8
+        }*/
+        //добавлено по мотивам 4.9. Создаем объект анонимного класса
+        //Comparator<? super ContactData> byId = (Comparator<ContactData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+        //int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId(); //добавлено по мотивам 4.9. Исполь-ем анонимную функцию
+
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId()); //добавлено по мотивам 4.8
+        //after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId() - лямда-выражение реализовано по мотивам 4.9
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));//сверка списков после добавл. нового контакта(по мотивам 4.7, 4.8)
         // с помощью HashSet<Object> преобразуем наши списки во множетсва, дабы не морочиться с порядком элементов в списке (по мотивам 4.8)
