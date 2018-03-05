59a7f263f13cd540b3b132102c2c66741aa9aed0
adashev <dimahelper@mail.ru>
2018-03-05 21:15:14 +0300

    Лекция 6.3 (контакты)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,4 +1,7 @@
 package ru.stqa.pft.addressbook.generators;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -10,16 +13,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
+
+    @Parameter(names = "-c", description = "Contact count")  //в 6.3
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")  //в 6.3
+    public String file;
+
     public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);//в 6.2
-        File file = new File(args[1]);//в 6.2
-
-        List<ContactData> contacts = generateСontacts(count);// 1) генерируем требуемое кол-во контактов
-        save(contacts, file);// 2) сохраняем сгенирированные контакты в файл
-
+        ContactDataGenerator generator = new ContactDataGenerator();//в 6.3
+        JCommander jCommander = new JCommander(generator);//в 6.3
+        try { //в 6.3
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+        /*int count = Integer.parseInt(args[0]);//в 6.2
+        File file = new File(args[1]);//в 6.2*/
     }
 
-    private static void save(List<ContactData> contacts, File file) throws IOException {//в 6.2
+    private void run() throws IOException { //в 6.3
+        List<ContactData> contacts = generateСontacts(count);// 1) генерируем требуемое кол-во контактов
+        save(contacts, new File(file));// 2) сохраняем сгенирированные контакты в файл
+    }
+
+    private void save(List<ContactData> contacts, File file) throws IOException {//в 6.2
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (ContactData contact : contacts) {
@@ -28,7 +48,7 @@ public class ContactDataGenerator {
         writer.close();
     }
 
-    private static List<ContactData> generateСontacts(int count) {//в 6.2
+    private List<ContactData> generateСontacts(int count) {//в 6.2
         List<ContactData> contacts = new ArrayList<ContactData>();
         for (int i = 0; i < count; i++) {
             contacts.add(new ContactData().withLastname(String.format("Lastname %s", i))
