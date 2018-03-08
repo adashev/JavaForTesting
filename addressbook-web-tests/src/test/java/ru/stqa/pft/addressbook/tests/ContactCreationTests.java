86a5f2727ee0933d6e9ddb5ffb53c6ffb82feea1
adashev <dimahelper@mail.ru>
2018-03-08 20:39:05 +0300

    Задание №13: Параметризовать тесты для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,14 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,18 +19,24 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
     @DataProvider // загружаем данные (6.4)
     public Iterator<Object[]> validContacts() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
+        //List<Object[]> list = new ArrayList<Object[]>();
         /*list.add(new Object[] {new ContactData().withFirstname("Firstn 1").withLastname("Lastn 1").withAddress("Addr д. 1")});
         list.add(new Object[] {new ContactData().withFirstname("Firstn 2").withLastname("Lastn 2").withAddress("Addr д. 2")});
         list.add(new Object[] {new ContactData().withFirstname("Firstn 3").withLastname("Lastn 3").withAddress("Addr д. 3")});*/
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));//6.5
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));//6.5
+
+        String xml = ""; // в 6.6
+
         String line = reader.readLine();
-        while (line != null) {// в 6.5
-            String[] split = line.split(";");
-            list.add(new Object[] {new ContactData().withLastname(split[0]).withFirstname(split[1]).withAddress(split[2])});
+        while (line != null) {// в 6.5. Изменено на xml в 6.6
+            xml += line;
             line = reader.readLine();
         }
-        return list.iterator();
+        XStream xstream = new XStream();// в 6.6
+        xstream.processAnnotations(ContactData.class);// в 6.6
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);// в 6.6
+        //return list.iterator();
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();// в 6.6;
     }
 
     @Test(dataProvider = "validContacts")
