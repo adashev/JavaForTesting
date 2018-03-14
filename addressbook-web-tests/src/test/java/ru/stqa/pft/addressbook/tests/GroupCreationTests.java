daaaf68bb0f4fee60898f9aad30498b8a659cd3c
adashev <dimahelper@mail.ru>
2018-03-14 20:33:54 +0300

    Лекция 6.11. Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -35,6 +37,7 @@ public class GroupCreationTests extends TestBase {
 
     @Test(dataProvider = "validGroups")
     public void testGroupCreation(GroupData group) {
+
         app.goTo().groupPage();
         Groups before = app.group().all();//считываем текущий список групп на странице до созданий новой группы (4.5)
         app.group().create(group);// null подставляем, т.к. хотим оставить в этих полях знач-я по умолч. (лекция 3.5)
@@ -44,6 +47,7 @@ public class GroupCreationTests extends TestBase {
         assertThat(after.size(), equalTo(before.size() + 1));
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class); // 6.11
+
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
             //= new ApplicationManager(BrowserType.CHROME);
@@ -18,8 +26,18 @@ public class TestBase {
         app.init();
     }
 
-    //@AfterSuite // вместо @BeforeMethod в 5.1
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object [] p){ // в Object [] p - параметры тестового метода
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p)); // 6.11
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m, Object [] p){
+        logger.info("Stop test " + m.getName() + " with parameters " + Arrays.asList(p)); // 6.11
     }
 }
