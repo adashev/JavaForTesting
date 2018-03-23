c72dc98f9b336ba5a88dd6f1409c021b463dec6b
adashev <dimahelper@mail.ru>
2018-03-23 18:50:05 +0300

    Лекция 9.2. SOAP: простой пример. Промежуточный. - 6:20

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,19 +1,24 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
-public class RegistrationHelper {
+public class RegistrationHelper extends HelperBase { // добавлен в 8.5
 
-    private final ApplicationManager app;
-    private WebDriver wd;
+    /*private final ApplicationManager app;
+    private WebDriver wd;*/ // закоментино в 8.6
 
     public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
-        wd = app.getDriver(); // делаем ленивую инициализацию драйвера
+        super(app); // вызываем конструктор базового класса (добавили в 8.6 -23:25)
+        //this.app = app;  закоментино в 8.6
+        //wd = app.getDriver(); // делали ленивую инициализацию драйвера. акоментино в 8.6
     }
 
     public void start(String username, String email) {
         wd.get(app.getProperty("web.baseUrl")+"/signup_page.php");
+        type(By.name("username"),username);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[value='Signup']")); // 8.6  -21:10
+
 
     }
 }
