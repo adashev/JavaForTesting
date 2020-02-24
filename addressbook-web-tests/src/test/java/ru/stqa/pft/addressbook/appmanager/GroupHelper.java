72e825ab4b1e9ddbbb1103aaf6bc0a926a508f9d
Dima <dimahelper@mail.ru>
2020-02-24 12:34:59 +0300

    4.3 - 3:25

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -6,52 +6,56 @@ import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupHelper extends HelperBase {
 
-    public GroupHelper(WebDriver wd) {
-        super(wd);
-    }
+  public GroupHelper(WebDriver wd) {
+    super(wd);
+  }
 
-    public void returnGroupPage() {
-        click(By.linkText("group page"));
-    }
+  public void returnGroupPage() {
+    click(By.linkText("group page"));
+  }
 
-    public void submitGroupCreation() {
-        click(By.name("submit"));
-    }
+  public void submitGroupCreation() {
+    click(By.name("submit"));
+  }
 
-    public void fillGroupForm(GroupData groupData) {
-        type(By.name("group_name"), groupData.getName());
-        type(By.name("group_header"), groupData.getHeader());
-        type(By.name("group_footer"), groupData.getFooter());
-    }
+  public void fillGroupForm(GroupData groupData) {
+    type(By.name("group_name"), groupData.getName());
+    type(By.name("group_header"), groupData.getHeader());
+    type(By.name("group_footer"), groupData.getFooter());
+  }
 
-    public void initGroupCreation() {
-        click(By.name("new"));
-    }
+  public void initGroupCreation() {
+    click(By.name("new"));
+  }
 
-    public void deleteSelectedGroups() {
-        click(By.xpath("//div[@id='content']/form/input[5]"));
-    }
+  public void deleteSelectedGroups() {
+    click(By.xpath("//div[@id='content']/form/input[5]"));
+  }
 
-    public void selectGroup() {
-        click(By.name("selected[]"));
-    }
+  public void selectGroup() {
+    click(By.name("selected[]"));
+  }
 
-    public void initGroupModification() {
-        click(By.name("edit"));
-    }
+  public void initGroupModification() {
+    click(By.name("edit"));
+  }
 
-    public void submitGroupModification() {
-        click(By.name("update"));
-    }
+  public void submitGroupModification() {
+    click(By.name("update"));
+  }
 
-    public void createGroup(GroupData group) {
-        initGroupCreation();
-        fillGroupForm(group);
-        submitGroupCreation();
-        returnGroupPage();
-    }
+  public void createGroup(GroupData group) {
+    initGroupCreation();
+    fillGroupForm(group);
+    submitGroupCreation();
+    returnGroupPage();
+  }
 
-    public boolean isThereAGroup() {
-        return isElementPresent(By.name("selected[]"));
-    }
+  public boolean isThereAGroup() {
+    return isElementPresent(By.name("selected[]"));
+  }
+
+  public int getGroupCount() {
+    return wd.findElements(By.name("selected[]")).size();
+  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
@@ -9,11 +10,11 @@ public class TestBase {
     protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod
-    public void setUp() throws Exception {
+    public void setUp() {
         app.init();
     }
 
-    //@AfterMethod
+    @AfterMethod
     public void tearDown() {
         app.stop();
     }
