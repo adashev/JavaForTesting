e2d13dabae0c2efed88821ab212f9715a54578fd
Dima <dimahelper@mail.ru>
2020-02-24 19:18:10 +0300

    4.5 - 2:10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,15 +4,19 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
+    List<GroupData> beforeGroupList = app.getGroupHelper().getGroupList();
     int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    List<GroupData> afterGroupList = app.getGroupHelper().getGroupList();
     int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before+1);
+    Assert.assertEquals(afterGroupList.size(), beforeGroupList.size()+1);
 
 
   }
