53ee48afadeb409d2d16c15bfd152680eba3a776
Dima <dimahelper@mail.ru>
2020-02-24 13:32:26 +0300

    4.3 - 5:13

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,19 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
-
-    @Test
-    public void testGroupDeletion() {
-        app.getNavigationHelper().gotoGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()){
-            app.getGroupHelper().createGroup(new GroupData("test10", null, null));
-        }
-        app.getGroupHelper().selectGroup();
-        app.getGroupHelper().deleteSelectedGroups();
-        app.getGroupHelper().returnGroupPage();
+  @Test
+  public void testGroupDeletion() {
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test10", null, null));
     }
+    int before = app.getGroupHelper().getGroupCount();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteSelectedGroups();
+    app.getGroupHelper().returnGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before-1);
+  }
 
 }
