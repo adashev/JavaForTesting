4c42b8be90bb1790d6dda1d419344c555f4f3041
Dima <dimahelper@mail.ru>
2020-04-11 18:52:38 +0300

    033 Лямбда - выражения часть I. 10:52

--- addressbook-web-tests/src/main/java/about/lambda/LambdaAdvance.java
+++ addressbook-web-tests/src/main/java/about/lambda/LambdaAdvance.java
@@ -1,32 +1,33 @@
 package about.lambda;
 
-
-interface Executable {
-  void execute();
-}
+interface Executable {  void execute(); }
 
 class Runner{
-  public void run(Executable e){
-
-  }
+   public void run(Executable e){ //ждет как аргумент реализацию интерфейса Executable
+      e.execute();
+   }
 }
 
-class ExecutableImplements implements Executable {
-  @Override
-  public void execute() {
-    System.out.println("hello");
-  }
+class ExecutableImplements implements Executable {//1-й самый многословный способ
+   @Override
+   public void execute() {
+      System.out.println("from ExecutableImplements override execute()");
+   }
 }
 
 public class LambdaAdvance {
-  public static void main(String[] args) {
-    Runner runner = new Runner();
-    runner.run(new ExecutableImplements());
-    runner.run(new Executable() {
-      @Override
-      public void execute() {
-        System.out.println("hello");
-      }
-    });
-  }
+   public static void main(String[] args) {
+      Runner runner = new Runner();
+      runner.run(new ExecutableImplements());//аргументом передаем реализацию интерф. Executable
+//применение 1-го способа
+
+      runner.run(new Executable() {//2-й способ - реализовать прямо здесь анонимный класс (не создавая class Runner)
+         @Override
+         public void execute() {
+            System.out.println("from anonim class");
+         }
+      }) ;
+
+      runner.run(() -> System.out.println("from lambda-ex"));
+   }
 }
